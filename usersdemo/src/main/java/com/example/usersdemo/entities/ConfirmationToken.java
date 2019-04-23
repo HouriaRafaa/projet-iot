4912b32b83abbaeb90f4d32beadb87fcce8c4c00
Houriaaa <rafaa.houria@gmail.com>
2019-04-23 17:32:24 +0200

    Update ConfirmationToken.java

--- usersdemo/src/main/java/com/example/usersdemo/entities/ConfirmationToken.java
+++ usersdemo/src/main/java/com/example/usersdemo/entities/ConfirmationToken.java
@@ -23,7 +23,7 @@ import javax.persistence.TemporalType;
 public class ConfirmationToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="token_id")
     private long tokenid;
 
