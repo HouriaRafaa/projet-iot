3d41d7afc09ec8427b3c427181f7f8ffecf5ec1d
Houriaaa <rafaa.houria@gmail.com>
2019-04-23 16:32:36 +0200

    Create ConfirmationToken.java

---
+++ usersdemo/src/main/java/com/example/usersdemo/entities/ConfirmationToken.java
@@ -0,0 +1,47 @@
+package com.example.usersdemo.entities;
+
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity @Data @NoArgsConstructor
+public class ConfirmationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="token_id")
+    private long tokenid;
+
+    @Column(name="confirmation_token")
+    private String confirmationToken;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @OneToOne(targetEntity = AppUser.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private AppUser user;
+
+    public ConfirmationToken(AppUser user) {
+        this.user = user;
+        createdDate = new Date();
+        confirmationToken = UUID.randomUUID().toString();
+    }
+
+
+}
