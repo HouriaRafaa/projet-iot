0137019e24b362654cdac981850f539474a3bfbd
Houriaaa <rafaa.houria@gmail.com>
2019-04-23 16:51:52 +0200

    Update UserController.java

--- usersdemo/src/main/java/com/example/usersdemo/web/UserController.java
+++ usersdemo/src/main/java/com/example/usersdemo/web/UserController.java
@@ -1,12 +1,18 @@
 package com.example.usersdemo.web;
 
 
+import com.example.usersdemo.dao.AppUserRepository;
+import com.example.usersdemo.dao.ConfirmationTokenRepository;
 import com.example.usersdemo.entities.AppUser;
+import com.example.usersdemo.entities.ConfirmationToken;
 import com.example.usersdemo.service.AccountService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class UserController {
@@ -14,13 +20,35 @@ public class UserController {
     @Autowired
     private AccountService accountService;
 
+    @Autowired
+    private ConfirmationTokenRepository confirmationTokenRepository;
+
+    @Autowired
+    private AppUserRepository userRepository;
+
 
     @PostMapping("/register")
     public AppUser register(@RequestBody UserForm userForm)
     {
-        return accountService.saveUser(userForm.getUsername(),userForm.getPassword(),userForm.getPasswordConfirmed());
+        return accountService.saveUser(userForm.getUsername(),userForm.getEmail(),userForm.getPassword(),userForm.getPasswordConfirmed());
     }
 
+
+    @RequestMapping(value="/confirm-account", method= {RequestMethod.GET, RequestMethod.POST})
+    public String confirmUserAccount(@RequestParam("token")String confirmationToken)
+    {
+        ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
+
+        if(token != null)
+        {
+            AppUser user = userRepository.findByEmailIgnoreCase(token.getUser().getEmail());
+            user.setActived(true);
+            userRepository.save(user);
+
+        }
+
+        return "Compte Verifié ^__^";
+    }
 
 
     @RequestMapping(value = "/id", method = RequestMethod.GET)
@@ -34,38 +62,13 @@ public class UserController {
 
 }
 
-@Data
+@Data @NoArgsConstructor @AllArgsConstructor
 class UserForm{
     private String username;
+    private String email;
     private  String password;
     private String passwordConfirmed;
 
-
-
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPasswordConfirmed() {
-        return passwordConfirmed;
-    }
-
-    public void setPasswordConfirmed(String passwordConfirmed) {
-        this.passwordConfirmed = passwordConfirmed;
-    }
 }
 
 
